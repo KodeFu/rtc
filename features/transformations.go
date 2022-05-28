bf66bee5f716c79c3db2a02b526df63fa1296417
Mudit Vats <muditv@gmail.com>
2022-05-28 05:50:43 -0700

    Added Rotation functions

--- features/transformations.go
+++ features/transformations.go
@@ -1,5 +1,7 @@
 package features
 
+import "math"
+
 func Translation(x, y, z float64) Matrix {
 	m := NewMatrix4x4([4][4]float64{{1, 0, 0, x}, {0, 1, 0, y}, {0, 0, 1, z}, {0, 0, 0, 1}})
 
@@ -8,6 +10,32 @@ func Translation(x, y, z float64) Matrix {
 
 func Scaling(x, y, z float64) Matrix {
 	m := NewMatrix4x4([4][4]float64{{x, 0, 0, 0}, {0, y, 0, 0}, {0, 0, z, 0}, {0, 0, 0, 1}})
+
+	return m
+}
+
+func Rotation_X(r float64) Matrix {
+	m := NewMatrix4x4([4][4]float64{{1, 0, 0, 0}, {0, math.Cos(r), -math.Sin(r), 0}, {0, math.Sin(r), math.Cos(r), 0}, {0, 0, 0, 1}})
+
+	return m
+}
+
+func Rotation_Y(r float64) Matrix {
+	m := NewMatrix4x4([4][4]float64{
+		{math.Cos(r), 0, math.Sin(r), 0},
+		{0, 1, 0, 0},
+		{-math.Sin(r), 0, math.Cos(r), 0},
+		{0, 0, 0, 1}})
+
+	return m
+}
+
+func Rotation_Z(r float64) Matrix {
+	m := NewMatrix4x4([4][4]float64{
+		{math.Cos(r), -math.Sin(r), 0, 0},
+		{math.Sin(r), math.Cos(r), 0, 0},
+		{0, 0, 1, 0},
+		{0, 0, 0, 1}})
 
 	return m
 }
--- features/transformations_test.go
+++ features/transformations_test.go
@@ -1,6 +1,7 @@
 package features
 
 import (
+	"math"
 	"testing"
 )
 
@@ -79,5 +80,78 @@ func TestScalingMultiplyInverse(testing *testing.T) {
 
 	if !result.Equals(expected) {
 		testing.Errorf("unexpected result %v %v", result, expected)
+	}
+}
+
+func TestRotatingPointAroundXAxis(testing *testing.T) {
+	var p = NewPoint(0, 1, 0)
+	var half_quarter = Rotation_X(math.Pi / 4)
+	var full_quarter = Rotation_X(math.Pi / 2)
+
+	var result_half_quarter = half_quarter.MultiplyByTuple(p)
+	var result_full_quarter = full_quarter.MultiplyByTuple(p)
+
+	var expected_half_quarter = NewPoint(0, math.Sqrt(2)/2, math.Sqrt(2)/2)
+	var expected_full_quarter = NewPoint(0, 0, 1)
+
+	if !result_half_quarter.Equals(expected_half_quarter) {
+		testing.Errorf("unexpected result %v %v", result_half_quarter, expected_half_quarter)
+	}
+
+	if !result_full_quarter.Equals(expected_full_quarter) {
+		testing.Errorf("unexpected result %v %v", result_full_quarter, expected_full_quarter)
+	}
+}
+
+func TestRotatingPointAroundXAxisOpposite(testing *testing.T) {
+	var p = NewPoint(0, 1, 0)
+	var half_quarter = Rotation_X(math.Pi / 4)
+	var inv = half_quarter.Inverse()
+
+	var result_half_quarter = inv.MultiplyByTuple(p)
+	var expected_half_quarter = NewPoint(0, math.Sqrt(2)/2, -math.Sqrt(2)/2)
+
+	if !result_half_quarter.Equals(expected_half_quarter) {
+		testing.Errorf("unexpected result %v %v", result_half_quarter, expected_half_quarter)
+	}
+}
+
+func TestRotatingPointAroundYAxis(testing *testing.T) {
+	var p = NewPoint(0, 0, 1)
+	var half_quarter = Rotation_Y(math.Pi / 4)
+	var full_quarter = Rotation_Y(math.Pi / 2)
+
+	var result_half_quarter = half_quarter.MultiplyByTuple(p)
+	var result_full_quarter = full_quarter.MultiplyByTuple(p)
+
+	var expected_half_quarter = NewPoint(math.Sqrt(2)/2, 0, math.Sqrt(2)/2)
+	var expected_full_quarter = NewPoint(1, 0, 0)
+
+	if !result_half_quarter.Equals(expected_half_quarter) {
+		testing.Errorf("unexpected result %v %v", result_half_quarter, expected_half_quarter)
+	}
+
+	if !result_full_quarter.Equals(expected_full_quarter) {
+		testing.Errorf("unexpected result %v %v", result_full_quarter, expected_full_quarter)
+	}
+}
+
+func TestRotatingPointAroundZAxis(testing *testing.T) {
+	var p = NewPoint(0, 1, 0)
+	var half_quarter = Rotation_Z(math.Pi / 4)
+	var full_quarter = Rotation_Z(math.Pi / 2)
+
+	var result_half_quarter = half_quarter.MultiplyByTuple(p)
+	var result_full_quarter = full_quarter.MultiplyByTuple(p)
+
+	var expected_half_quarter = NewPoint(-math.Sqrt(2)/2, math.Sqrt(2)/2, 0)
+	var expected_full_quarter = NewPoint(-1, 0, 0)
+
+	if !result_half_quarter.Equals(expected_half_quarter) {
+		testing.Errorf("unexpected result %v %v", result_half_quarter, expected_half_quarter)
+	}
+
+	if !result_full_quarter.Equals(expected_full_quarter) {
+		testing.Errorf("unexpected result %v %v", result_full_quarter, expected_full_quarter)
 	}
 }
